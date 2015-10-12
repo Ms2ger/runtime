8d209707771c92f8288a1d285a9c25b2f0e9909f
Ms2ger <Ms2ger@gmail.com>
2015-10-12 11:33:17 +0200

    Turn on clippy.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![feature(plugin)]
+#![feature(plugin_registrar)]
+#![feature(rustc_private)]
+
+#![plugin(clippy)]
+
+extern crate clippy;
 extern crate js;
+extern crate rustc;
+
+use rustc::plugin::Registry;
+
+#[plugin_registrar]
+pub fn plugin_registrar(registry: &mut Registry) {
+    clippy::plugin_registrar(registry);
+}
 
 fn main() {
     println!("Hello, world!");
